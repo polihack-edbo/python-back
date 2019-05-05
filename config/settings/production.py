f5c1806c86c1301b2b3c0fc890a7057ee6a05f84
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-05-05 10:58:06 -0500

    Added Heroku debugging option

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 SECRET_KEY = os.environ['SECRET_KEY']
 ENVIRONMENT = 'production'
-DEBUG = False
+DEBUG = os.environ['DEBUG'] if 'DEBUG' in os.environ else False
 ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(
     default='DATABASE_URL_HERE'

a921c51b0be5ed5f406d931a067327ed94f29d6f
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-05-05 10:53:52 -0500

    Added support for media files

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('users.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
